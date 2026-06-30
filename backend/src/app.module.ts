87ee1f4592f5f4b640d7e932e21b818a9dd91757
FamilyGuard <oo01029829517@gmail.com>
2026-06-30 11:02:15 +0300

    Add FamiliesModule to AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { FamiliesModule } from './families/families.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule,
+    FamiliesModule,
   ],
 })
 export class AppModule {}
