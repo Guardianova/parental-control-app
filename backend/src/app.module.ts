bdeb495d25d0dee97fbdf7d94ff314cda76e1f4f
FamilyGuard <oo01029829517@gmail.com>
2026-06-30 11:09:23 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { FamiliesModule } from './families/families.module';
+import { ChildrenModule } from './children/children.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { FamiliesModule } from './families/families.module';
     PrismaModule,
     AuthModule,
     FamiliesModule,
+    ChildrenModule,
   ],
 })
 export class AppModule {}
