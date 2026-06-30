ca05b859006dde73e486b0de2fa8824ff2f25d41
FamilyGuard <oo01029829517@gmail.com>
2026-06-30 15:39:19 +0300

    Create main.dart

---
+++ parent_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: ParentApp()));
+}
+
+class ParentApp extends StatelessWidget {
+  const ParentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'رقابة أبوية - ولي الأمر',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.indigo,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Parent App - جاهز للبدء'),
+        ),
+      ),
+    );
+  }
+}
